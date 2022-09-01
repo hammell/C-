f36a2dfefc88b47d7a4c3e731f5f0e90ee1091d6
Alexandr <hammell@ya.ru>
2022-09-01 23:10:45 +0800

    add random

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10) ; // целое число от 1 2 3 4 ...9 (max = max - 1)
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10) ; // целое число от 1 2 3 4 ...9 (max = max - 1)
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
